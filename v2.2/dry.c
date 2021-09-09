88b75f847054f1f3a8c4de7d8aca8ef7f06c11ca
notcarbide <luis@0k.cx>
2021-09-09 04:57:23 +0100

    dry: Increase run values
    Because all other results are meaningless (at least on cortex-a72).
    
    Signed-off-by: notcarbide <luis@0k.cx>

--- v2.2/dry.c
+++ v2.2/dry.c
@@ -11,17 +11,17 @@
 	      ${CC}    -c ${CFLAGS} $0 -o dry1.o || exit 1
 	echo "${CC} -DPASS2 ${CFLAGS} $0 dry1.o ${LFLAGS} -o dry2"
 	      ${CC} -DPASS2 ${CFLAGS} $0 dry1.o ${LFLAGS} -o dry2 || exit 1
-	./dry2 ${1-50000} 2>/dev/null
+	./dry2 ${1-50000000} 2>/dev/null
 	echo "${CC=cc} -c -DREG ${CFLAGS} $0 -o dry1.o"
 	      ${CC}    -c -DREG ${CFLAGS} $0 -o dry1.o || exit 1
 	echo "${CC} -DPASS2 -DREG ${CFLAGS} $0 dry1.o ${LFLAGS} -o dry2nr"
 	      ${CC} -DPASS2 -DREG ${CFLAGS} $0 dry1.o ${LFLAGS} -o dry2nr || exit 1
-	./dry2nr ${1-50000} 2>/dev/null
+	./dry2nr ${1-50000000} 2>/dev/null
 	echo "${CC=cc} -c -O ${CFLAGS} $0 -o dry1.o"
 	      ${CC}    -c -O ${CFLAGS} $0 -o dry1.o || exit 1
 	echo "${CC} -DPASS2 -O ${CFLAGS} $0 dry1.o ${LFLAGS} -o dry2o"
 	      ${CC} -DPASS2 -O ${CFLAGS} $0 dry1.o ${LFLAGS} -o dry2o || exit 1
-	./dry2o ${1-50000} 2>/dev/null
+	./dry2o ${1-50000000} 2>/dev/null
 	rm -f dry1.o
 
         exit 0
@@ -431,7 +431,7 @@ struct tms      time_info;
 
 
 #define Mic_secs_Per_Second     1000000.0
-#define NUMBER_OF_RUNS		50000 /* Default number of runs */
+#define NUMBER_OF_RUNS		50000000 /* Default number of runs */
 
 #ifdef  NOSTRUCTASSIGN
 #define structassign(d, s)      memcpy(&(d), &(s), sizeof(d))
